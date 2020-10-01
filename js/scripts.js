68b52f3a191543b9d3f81142713acfd9f96ae11d
Sam Welek <s.welek@outlook.com>
2020-10-01 22:21:39 +0100

    Restyling home page, cleaning up migration

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,24 @@
 const sections = [...document.querySelectorAll("section")];
 
 let options = {
-  rootMargin: "0px",
-  threshold: 0.25
+	rootMargin: "0px",
+	threshold: 0.25
 };
 
 const callback = (entries, observer) => {
-  entries.forEach(entry => {
+	entries.forEach(entry => {
 		const { target } = entry
-		
+
 		if (entry.intersectionRatio >= 0.25) {
 			target.classList.add("is-visible")
 		} else {
 			target.classList.remove("is-visible")
 		}
-  })
+	})
 }
 
 const observer = new IntersectionObserver(callback, options)
 
 sections.forEach((section, index) => {
-  observer.observe(section)
+	observer.observe(section)
 })
